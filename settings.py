d0f4da2d03fe517b3e331f445c652844be2e9870
TatyanaShevchuk <104738810+TatyanaShevchuk@users.noreply.github.com>
2022-12-11 23:29:30 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,12 @@
 email = 'shevtanya2022@gmail.com'
 password = 'kaDbaa'
 incorrect_password = '1111'
+
+exersice_name = 'Doing something'
+
+surname = 'Shevchuk'
+name = 'Tanya'
+patronymic = 'Aleksandrovna'
+city = 'Warsaw'
+address = 'Street'
+birthday = '01.02.2015'
